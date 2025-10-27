5544798d4368006f4f038f29bcd1a86c1188e3cf
ziller <143824815+godziller@users.noreply.github.com>
2025-10-27 20:13:10 +0000

    Update bitSelect-5.ino
    adding comments.

--- lecture9/bitSelect-5.ino
+++ lecture9/bitSelect-5.ino
@@ -10,11 +10,18 @@ void setup() {
 void loop() {
   PORTD = 0xff; //set all pins HIGH
   delay(1000);
+  // the ~ inverts so in this case you end up with 11011111
+  //which then wiht an & filters and sets bit 6 to 0
+  // Basically use &= coupled with ~ to set a bit low.
   PORTD &= ~(1<<BIT_TO_SELECT); // set BIT_TO_SELECT LOW
   delay(1000);
   PORTD = 0x00; //set all pins LOW
   delay(5000);
+
+  // kinda opposite to earlier, here you are using the OR function.
+  // to inside the () you get 00100000, then the |= just affects the target bit.
   PORTD |= (1<<BIT_TO_SELECT);  //set BIT_TO_SELECT HIGH
   delay (1000);
 }
+
 
