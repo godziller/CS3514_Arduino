0fc442a2d9640cac1ae6657fc236780cff1a77c1
ziller <143824815+godziller@users.noreply.github.com>
2025-10-28 16:22:05 +0000

    Update SinglePersonTimer.ino

--- lab5/SinglePersonTimer.ino
+++ lab5/SinglePersonTimer.ino
@@ -1,93 +1,222 @@
+// --- PORTB MACROS (Pins D8-D13) ---
+// Choosing to use the same port for button and led - PORTB.
+// But naming carefully with _B, so coder understands dealing with 
+// PORTB. 
+
+// Sets a bit in PORTB 
+#define SET_BIT_B(BIT) (PORTB |= (1 << BIT))
+
+// Clears a bit in PORTB low 
+#define CLR_BIT_B(BIT) (PORTB &= ~(1 << BIT))
+
+// Reads the state of a specific pin
+// True = High, False = Low.
+#define READ_BIT_B(BIT) (PINB & (1 << BIT))
+
+
 // --- PIN DEFINITIONS ---
-#define NumberOfLEDS 6
-#define BUTTON_BIT PD7    // Using D7 for the input switch
-#define ASSERT(expr) if (!(expr)) {printf("The Following Failed: %s\n", #expr); exit(1);}
+// Reflect how we wire our solution - Pin8 for LED, pin 13 for button.
+#define START_LED_BIT PB0   // Pin 8 (PB0) is the start signal LED.
+#define BUTTON_BIT PB5      // Pin 13 (PB5) is the button pin.
 
-// Going to use 0.5sec for each LED to turn off.
-#define FIXED_COUNTDOWN_DELAY 500 
+// Assignment Constraints for random number range
+// Used for random number/ wait time generation.
+#define WAIT_MIN 0       // Minimum random wait time (0 seconds)
+#define WAIT_MAX 10000   // Maximum random wait time (10 seconds)
 
-// Need a mask for  LEDS D8-D13 - which are on PORTB
-// Could just use B00111111 which maps to the PINS/LEDS
-// But try and make code nicer with bit shift and OR operator
-// PB0 = D8, PB1 = D9, ...PB5=D13
-#define LED_MASK ((1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4) | (1 << PB5))
- 
 
 // Variable for timing reaction.
 // Using big numbers here because we don't know how slow the user is.
-// Time moves forward, so always positive.
 unsigned long startTime = 0;
 unsigned long endTime = 0;
 unsigned long reactionTime = 0;
+unsigned long randomWaitTime = 0; // The calculated delay before GO signal
+
+// Max out initial best time so the first game run will be the best (hopefully!).
+unsigned long bestTime = 0xFFFFFFFF; 
+// Running counter for multiple game plays - needs to be big -i.e. unsigned long
+unsigned long totalReactionTime = 0;
+
+// Record the number of games played to help with the average calc
+int gameCount = 0;
+// used for average reaction time
+float averageReactionTime = 0.0;
+
+
+// Using a fucntion to keep code modular and readable.
+// this shoudl take in the current game result and update the 
+// game variables above, then print the running results...
+void reportResults(unsigned long currentReactionTime);
+
+// Added prototype for scoreboard reset function
+void resetScoreboard();
+
 
 void setup() {
-  // Using ASSERT from last week's solutoin to ensure working with correct
-  // number of bits.
-  ASSERT(NumberOfLEDS <= 6);
+  // Set up serial so we can see what's going on.
+  Serial.begin(9900); 
   
-  // Set all my LED PINS as output.
-  DDRB |= LED_MASK; 
+  // Arduino random function needs setup, so I need to initialize it
+  // here for use later. Using board time as the seed 'cos I can't use 
+  // the AnalogRead() recommended in arduino docs due to lab guidance.
+  randomSeed(micros()); 
   
-  // And we turn them all off at the start.
-  PORTB &= ~LED_MASK; 
+  Serial.println("Game ON!");
 
-  // Now set the Button/Switch PIN to an input
-  DDRD &= ~(1 << BUTTON_BIT); 
-  // and set it as pull up, so we declutter board not needing resistor
-  PORTD |= (1 << BUTTON_BIT); 
+  // Set up input and output configuration - LED->out, Button->in
+  DDRB |= (1 << START_LED_BIT); 
+  DDRB &= ~(1 << BUTTON_BIT);  
 
-  // Going to use Serial out to see what's going on.
-  Serial.begin(9600);
-  Serial.println("--- 6-LED Reaction Timer Ready ---");
+  // Need to turn off LED and enable button for PULLUP
+  CLR_BIT_B(START_LED_BIT);  
+  SET_BIT_B(BUTTON_BIT);   
 }
+
 
 void loop() {
   
-  // And we're OFF to the races!!
-  Serial.println("\nPress the button (D7) to start the countdown.");
+  // We begin with a print to screen
+  Serial.println("\nPress Button to start game!");
   
-  // Wait for the starting gun!!
-  while (PIND & (1 << BUTTON_BIT)) { 
-      // Wait forever for button pressed to start game.
+  // A loop waiting for user to press button.
+  while (READ_BIT_B(BUTTON_BIT)) {
+    // Using debounce because especially this game likely to happen.
+    delay(50); 
   }
   
-  // Using our LED mask, turn all LEDs on
-  PORTB |= LED_MASK; 
+  // Need this also because the player may have kept the button
+  // depressed. So this waits for the button to be released.
   
+  while (!READ_BIT_B(BUTTON_BIT)) { 
+    // Using debounce because especially this game likely to happen.
+    delay(50); 
+  }
 
-  Serial.println("Countdown started");
+  Serial.println("Ready... Keep eye on LED!!!.");
+
+  // Generate a random wait/backoff time and get a start time
+  // recorded for the game.
+  randomWaitTime = random(WAIT_MIN, WAIT_MAX); 
+  unsigned long randomStartTime = millis();
+
+  // Countdown loop, take current time from the recorded start time
+  // and see if it is less than what we wait. If true, keep waiting.
+  // otherwise move on
+  while (millis() - randomStartTime < randomWaitTime) {
+    
+    // Is while we are waiting here for the go signal
+    // that a player may falsely press the button.
+    // if they do that we need to clear out all the game stats data
+    // they lose big time!!
+    if (!READ_BIT_B(BUTTON_BIT)) {
+      // Ooops - too soon!!
+      Serial.println("\n!!! TOO IMPATIENT!!  !!!");
+      Serial.println("Looser!! All your game data in the bin!!.");
+      
+      // Reset all scoring variables as required by the update
+      resetScoreboard();
+      
+      // Need this also because the player may have kept the button
+      // depressed. So this waits for the button to be released.
+      while (!READ_BIT_B(BUTTON_BIT)) {
+          // Wait for release
+      } 
+      // random delay to exit
+      delay(5000); 
+      return; // Break out of this loop and go back to main loop.
+    }
+    // The code simply waits here for the random time to pass.
+  }
+
+  // Game ON! light LED to signal to player to react.
+  SET_BIT_B(START_LED_BIT); 
   
-  // 2. Countdown Sequence (Depleting: D8/PB0 is turned OFF first)
-  for (int i = 0; i < NumberOfLEDS; i++) {
-    
-    delay(FIXED_COUNTDOWN_DELAY); 
-    
-    // Walk down the LEDs and turn each off in theloop
-    PORTB &= ~(1 << i); 
-    
-  } // Loop finishes So now the timer /reaction starts.
-  
-  // Immediately after the last LED turns OFF, start the timer
+  // Immediately start the timer to time the user reaction.
   startTime = micros(); 
-  // Use serial out to let us know where we are too.
-  Serial.println("GO! (Last LED off - PRESS NOW!)");
-
-  // Loop while the button is UNPRESSED (PIND bit 7 is HIGH)
-  while (PIND & (1 << BUTTON_BIT)) {
-    // Going to sit looping around here doing
-    // nothing until button pressed.
+  Serial.println("GO! GO! GO!, PRESS BUTTON!!");
+  
+  
+  // Loop while the button is UNPRESSED (PINB bit 5 is HIGH)
+  while (READ_BIT_B(BUTTON_BIT)) {
+    // Breaking out of this signals Player has pressed button.
   }
 
-  // Ok button pressed, do calculations...
+  // Record the result.
   endTime = micros();
   reactionTime = endTime - startTime;
 
-  Serial.println("--- Reaction Complete ---");
+  // Turn the start signal LED OFF in prep for new game.
+  CLR_BIT_B(START_LED_BIT); 
+
+  // hand over to function to do the printing...
+  reportResults(reactionTime); 
+
+  // Hold off for a bit before starting new game.
+  delay(5000); 
+}
+
+
+// Takes in current score, updates the game count.
+// check for if this is the best time and updates.
+// calcs the average...
+void reportResults(unsigned long currentReactionTime) {
+  
+  // Update Game Count - need for proper average calc.
+  gameCount++;
+  
+  // same here for average calc
+  totalReactionTime += currentReactionTime;
+
+  // Running Average calc
+  averageReactionTime = (float)totalReactionTime / gameCount;
+  
+  // Record best time?? 
+  if (currentReactionTime < bestTime) {
+    bestTime = currentReactionTime;
+  }
+  
+  // Now to display part....
+  
+  // Banner
+  Serial.println("GAME OVER");
+  
+  // Display random wait time
+  Serial.print("Random Wait Time was: ");
+  // Note: randomWaitTime is a global variable
+  Serial.print(randomWaitTime); 
+  Serial.println(" ms");
+  
+  // Convert microseconds to milliseconds for display
+  // ealier use of millis were quite untidy, we we do a simple downshift
+  float currentReactionTimeMS = currentReactionTime / 1000.0;
+  float bestTimeMS = bestTime / 1000.0;
+  float averageTimeMS = averageReactionTime / 1000.0;
+  
   // Convert microseconds to milliseconds
-  Serial.print("Reaction Time: ");
-  Serial.print(reactionTime / 1000.0); 
+  Serial.print("REACTION TIME: ");
+  Serial.print(currentReactionTimeMS, 2); 
   Serial.println(" ms"); 
 
-  // Wait 5 seconds before allowing a new game
-  delay(5000); 
+  Serial.println("--- RESULTS ---");
+  Serial.print("BEST TIME (Total Games: ");
+  Serial.print(gameCount);
+  Serial.print("): ");
+  Serial.print(bestTimeMS, 2);
+  Serial.println(" ms");
+  
+  Serial.print("AVERAGE REACTION TIME: ");
+  Serial.print(averageTimeMS, 2);
+  Serial.println(" ms");
+}
+
+// New function definition for resetting scoring variables
+void resetScoreboard() {
+    // Max out initial best time so the next game run will be the best (hopefully!).
+    bestTime = 0xFFFFFFFF; 
+    // Running counter for multiple game plays - reset to 0
+    totalReactionTime = 0;
+    // Record the number of games played - reset to 0
+    gameCount = 0;
+    // used for average reaction time - reset to 0.0
+    averageReactionTime = 0.0;
 }
