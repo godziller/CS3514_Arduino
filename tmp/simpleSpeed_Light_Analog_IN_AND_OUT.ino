f6fc0e4c14b6f4d4705c0d4b27ad06882577a7f3
ziller <143824815+godziller@users.noreply.github.com>
2025-10-28 13:01:37 +0000

    Define PWM_MIN and PWM_MAX for LED brightness control
    Added PWM_MIN and PWM_MAX definitions for LED control.

--- tmp/simpleSpeed_Light_Analog_IN_AND_OUT.ino
+++ tmp/simpleSpeed_Light_Analog_IN_AND_OUT.ino
@@ -5,6 +5,9 @@
 #define MAX_DELAY 500 // time in ms
 #define MIN_SENSOR_VAL 90  //value when darkest
 #define MAX_SENSOR_VAL 800 //value when brightest
+#define PWM_MIN = 0   → LED always OFF
+#define PWM_MAX = 255 → LED mostly ON (appears fully bright)
+
 // PWM-capable pins on Arduino Uno
 int ledPins[NumberOfLEDS] = {3, 5, 6, 9, 10, 11};
 void setup() {
@@ -19,14 +22,19 @@ void setup() {
   DDRD |= (1<<PD3) | (1<<PD5) | (1<<PD6);  // Pins 3, 5, 6 are on PORTD
   DDRB |= (1<<PB1) | (1<<PB2) | (1<<PB3);  // Pins 9, 10, 11 are on PORTB
 }
+
 // Using function that takes in a pointer to the values we want modified
 // Note, analog read is done inside this function, just like last weeks solution
 // but we could do it once every loop, which would then modify this function to 
 // take a third 'sensorVal' argument that is per loop cycle.
+// However inside the function I will only use analogRead once to ensure
+// I'm synchronized between light and speed for the function call.
 void readSensorValues(unsigned int *delayTime, unsigned int *pwmValue) {
-  *delayTime = map(analogRead(A0), MIN_SENSOR_VAL, MAX_SENSOR_VAL, MAX_DELAY, MIN_DELAY);
-  *pwmValue = map(analogRead(A0), MIN_SENSOR_VAL, MAX_SENSOR_VAL, 0, 255);
+  // Cast map() results to unsigned int to prevent overflow issues
+  *delayTime = (unsigned int)map(sensorVal, MIN_SENSOR_VAL, MAX_SENSOR_VAL, MAX_DELAY, MIN_DELAY);
+  *pwmValue = (unsigned int)map(sensorVal, MIN_SENSOR_VAL, MAX_SENSOR_VAL, PWM_MIN, PWM_MAX);
 }
+
 void loop() {
   int i;
   // these variables are going to be adjusted per the A0 input value.
